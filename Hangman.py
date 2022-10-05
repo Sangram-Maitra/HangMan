364c2f7bc1567a8c898acff3722013b3e95d6303
Sangram Maitra <96986463+Sangram-Maitra@users.noreply.github.com>
2022-10-05 15:45:13 +0530

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,531 @@
+# #Step 1 
+# import random
+
+# word_list = ["aardvark", "baboon", "camel"]
+
+# #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# choosen_word=random.choice(word_list)
+# #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# guess=input("Enter the guess letter:")
+# guess=guess.lower()
+# #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# for i in range(len(choosen_word)):
+#     if guess==choosen_word[i]:
+#         print("Guess right")
+#     else:
+#         print("Guess wrong")
+
+
+
+# #Step 2
+
+# import random
+# word_list = ["aardvark", "baboon", "camel"]
+# chosen_word = random.choice(word_list)
+
+# #Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+# #TODO-1: - Create an empty List called display.
+# #For each letter in the chosen_word, add a "_" to 'display'.
+# #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+# answer=[]
+# final=[]
+# print(answer)
+
+# final=chosen_word
+# while chosen_word!=answer:
+#     guess = input("Guess a letter: ").lower()
+#     #TODO-2: - Loop through each position in the chosen_word;
+#     #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#     #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+#     for letter in chosen_word:
+#         if letter == guess:
+#             print("Right")
+#         else:
+#             print("Wrong")
+
+#     #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#     #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+#     print("Display ")
+#     for i in range(len(chosen_word)):
+#         if chosen_word[i]==guess: 
+#             answer+=guess
+
+#     print(answer)
+
+
+
+#Step 3
+
+# import random
+# word_list = ["aardvark", "baboon", "camel"]
+# chosen_word = random.choice(word_list)
+# word_length = len(chosen_word)
+
+# #Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+# #Create blanks
+# display = []
+# for _ in range(word_length):
+#     display += "_"
+
+# #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+# full_letter=display
+# # print(full_letter)
+# while "".join(full_letter)!=chosen_word:
+#     guess = input("Guess a letter: ").lower()
+#     #Check guessed letter
+#     for position in range(word_length):
+#         letter = chosen_word[position]
+#         print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+#         if letter == guess:
+#             display[position] = letter
+#             full_letter[position]=guess
+#     print(display)
+
+# ## Step 4
+
+# import random
+
+# stages = ['''
+#   +---+
+#   |   |
+#   O   |
+#  /|\  |
+#  / \  |
+#       |
+# =========
+# ''', '''
+#   +---+
+#   |   |
+#   O   |
+#  /|\  |
+#  /    |
+#       |
+# =========
+# ''', '''
+#   +---+
+#   |   |
+#   O   |
+#  /|\  |
+#       |
+#       |
+# =========
+# ''', '''
+#   +---+
+#   |   |
+#   O   |
+#  /|   |
+#       |
+#       |
+# =========''', '''
+#   +---+
+#   |   |
+#   O   |
+#   |   |
+#       |
+#       |
+# =========
+# ''', '''
+#   +---+
+#   |   |
+#   O   |
+#       |
+#       |
+#       |
+# =========
+# ''', '''
+#   +---+
+#   |   |
+#       |
+#       |
+#       |
+#       |
+# =========
+# ''']
+
+# end_of_game = False
+# word_list = ["ardvark", "baboon", "camel"]
+# chosen_word = random.choice(word_list)
+# word_length = len(chosen_word)
+
+# #TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+# #Set 'lives' to equal 6.
+# lives=6
+# #Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+# #Create blanks
+# display = []
+# for _ in range(word_length):
+#     display += "_"
+
+# while not end_of_game:
+#     guess = input("Guess a letter: ").lower()
+
+#     #Check guessed letter
+#     for position in range(word_length):
+#         letter = chosen_word[position]
+#         # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+#         if letter == guess:
+#             display[position] = letter
+#     if guess not in chosen_word:
+#         lives-=1
+#     #TODO-2: - If guess is not a letter in the chosen_word,
+#     #Then reduce 'lives' by 1. 
+#     #If lives goes down to 0 then the game should stop and it should print "You lose."
+#     if lives==0:
+#         print("You lose")
+#         end_of_game=True
+#     #Join all the elements in the list and turn it into a String.
+#     print(f"{' '.join(display)}")
+
+#     #Check if user has got all letters.
+#     if "_" not in display:
+#         end_of_game = True
+#         print("You win.")
+
+#     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+#     print(stages[lives])
+
+
+#Step 5
+
+import random
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+print(logo)
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+word_list = [
+'abruptly', 
+'absurd', 
+'abyss', 
+'affix', 
+'askew', 
+'avenue', 
+'awkward', 
+'axiom', 
+'azure', 
+'bagpipes', 
+'bandwagon', 
+'banjo', 
+'bayou', 
+'beekeeper', 
+'bikini', 
+'blitz', 
+'blizzard', 
+'boggle', 
+'bookworm', 
+'boxcar', 
+'boxful', 
+'buckaroo', 
+'buffalo', 
+'buffoon', 
+'buxom', 
+'buzzard', 
+'buzzing', 
+'buzzwords', 
+'caliph', 
+'cobweb', 
+'cockiness', 
+'croquet', 
+'crypt', 
+'curacao', 
+'cycle', 
+'daiquiri', 
+'dirndl', 
+'disavow', 
+'dizzying', 
+'duplex', 
+'dwarves', 
+'embezzle', 
+'equip', 
+'espionage', 
+'euouae', 
+'exodus', 
+'faking', 
+'fishhook', 
+'fixable', 
+'fjord', 
+'flapjack', 
+'flopping', 
+'fluffiness', 
+'flyby', 
+'foxglove', 
+'frazzled', 
+'frizzled', 
+'fuchsia', 
+'funny', 
+'gabby', 
+'galaxy', 
+'galvanize', 
+'gazebo', 
+'giaour', 
+'gizmo', 
+'glowworm', 
+'glyph', 
+'gnarly', 
+'gnostic', 
+'gossip', 
+'grogginess', 
+'haiku', 
+'haphazard', 
+'hyphen', 
+'iatrogenic', 
+'icebox', 
+'injury', 
+'ivory', 
+'ivy', 
+'jackpot', 
+'jaundice', 
+'jawbreaker', 
+'jaywalk', 
+'jazziest', 
+'jazzy', 
+'jelly', 
+'jigsaw', 
+'jinx', 
+'jiujitsu', 
+'jockey', 
+'jogging', 
+'joking', 
+'jovial', 
+'joyful', 
+'juicy', 
+'jukebox', 
+'jumbo', 
+'kayak', 
+'kazoo', 
+'keyhole', 
+'khaki', 
+'kilobyte', 
+'kiosk', 
+'kitsch', 
+'kiwifruit', 
+'klutz', 
+'knapsack', 
+'larynx', 
+'lengths', 
+'lucky', 
+'luxury', 
+'lymph', 
+'marquis', 
+'matrix', 
+'megahertz', 
+'microwave', 
+'mnemonic', 
+'mystify', 
+'naphtha', 
+'nightclub', 
+'nowadays', 
+'numbskull', 
+'nymph', 
+'onyx', 
+'ovary', 
+'oxidize', 
+'oxygen', 
+'pajama', 
+'peekaboo', 
+'phlegm', 
+'pixel', 
+'pizazz', 
+'pneumonia', 
+'polka', 
+'pshaw', 
+'psyche', 
+'puppy', 
+'puzzling', 
+'quartz', 
+'queue', 
+'quips', 
+'quixotic', 
+'quiz', 
+'quizzes', 
+'quorum', 
+'razzmatazz', 
+'rhubarb', 
+'rhythm', 
+'rickshaw', 
+'schnapps', 
+'scratch', 
+'shiv', 
+'snazzy', 
+'sphinx', 
+'spritz', 
+'squawk', 
+'staff', 
+'strength', 
+'strengths', 
+'stretch', 
+'stronghold', 
+'stymied', 
+'subway', 
+'swivel', 
+'syndrome', 
+'thriftless', 
+'thumbscrew', 
+'topaz', 
+'transcript', 
+'transgress', 
+'transplant', 
+'triphthong', 
+'twelfth', 
+'twelfths', 
+'unknown', 
+'unworthy', 
+'unzip', 
+'uptown', 
+'vaporize', 
+'vixen', 
+'vodka', 
+'voodoo', 
+'vortex', 
+'voyeurism', 
+'walkway', 
+'waltz', 
+'wave', 
+'wavy', 
+'waxy', 
+'wellspring', 
+'wheezy', 
+'whiskey', 
+'whizzing', 
+'whomever', 
+'wimpy', 
+'witchcraft', 
+'wizard', 
+'woozy', 
+'wristwatch', 
+'wyvern', 
+'xylophone', 
+'yachtsman', 
+'yippee', 
+'yoked', 
+'youthful', 
+'yummy', 
+'zephyr', 
+'zigzag', 
+'zigzagging', 
+'zilch', 
+'zipper', 
+'zodiac', 
+'zombie', 
+]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+
+#Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+import os
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    os.system("cls")
+    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in display:
+        print("You have already enter this letter")
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print("You guess wrong letter,you lose 1 life")
+        lives -= 1
+        print(f"Now you have {lives}lives")
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    print(stages[lives])  
